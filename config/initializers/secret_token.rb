d9f147f10de43304e3813dcc41abaf23dcdd6eda
forni <forni@forni-VGN-C190G.(none)>
2011-11-18 15:56:23 -0800

    inital commit - vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piecoras::Application.config.secret_token = '850e4f3a9b3e47a893dcb990f6f7983bf0c11f68285937ef273d23a32adf59650fdab8274a31aa2fe9a8b7e8cbbd7149af027bbde464ff1bbc9cda5aa4e12b45'
